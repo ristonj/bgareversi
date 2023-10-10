d08a6965fecc0f0e91b50b17b0c7d8bfe1f4791c
John Riston <ristonj@gmail.com>
2023-10-09 22:47:25 -0400

    Added initial token.

--- reversiristonj.js
+++ reversiristonj.js
@@ -57,7 +57,7 @@ function (dojo, declare) {
             }
             
             // TODO: Set up your game interface here, according to "gamedatas"
-            
+            this.addTokenOnBoard( 2, 2, this.player_id );
  
             // Setup game notifications to handle (see "setupNotifications" method below)
             this.setupNotifications();
@@ -157,7 +157,16 @@ function (dojo, declare) {
             script.
         
         */
-
+        addTokenOnBoard: function( x, y, player )
+        {
+            dojo.place( this.format_block( 'jstpl_token', {
+                x_y: x+'_'+y,
+                color: this.gamedatas.players[ player ].color
+            } ) , 'tokens' );
+            
+            this.placeOnObject( 'token_'+x+'_'+y, 'overall_player_board_'+player );
+            this.slideToObject( 'token_'+x+'_'+y, 'square_'+x+'_'+y ).play();
+        },
 
         ///////////////////////////////////////////////////
         //// Player's action
--- reversiristonj_reversiristonj.tpl
+++ reversiristonj_reversiristonj.tpl
@@ -30,18 +30,15 @@
     <!-- BEGIN square -->
         <div id="square_{X}_{Y}" class="square" style="left: {LEFT}px; top: {TOP}px;"></div>
     <!-- END square -->
+    <div id="tokens">
+    </div>
  </div>
 
 
 <script type="text/javascript">
 
 // Javascript HTML templates
-
-/*
-// Example:
-var jstpl_some_game_item='<div class="my_game_item" id="my_game_item_${MY_ITEM_ID}"></div>';
-
-*/
+var jstpl_token='<div class="token tokencolor_${color}" id="token_${x_y}"></div>';
 
 </script>  
 
